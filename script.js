f1185c9a988b50270bf30aad64a243889b7b03f5
Denise Kurek <denisekurek@rocketmail.com>
2022-02-16 11:17:09 -0500

    Got JavaScript working to allow for changes in screens between teams. Displays move in and out of view with the right click.

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const uba = document.getElementById("backButtonUBA");
-const fed = document.getElementById("backButtonFED");
+const m2x = document.getElementById('M2X');
+const cf = document.getElementById('CF');
+const ubaScores = document.getElementById('ubaScores');
+const fedScores = document.getElementById('fedScores');
+const uba = document.getElementById('backButtonUBA');
+const fed = document.getElementById('backButtonFED');
+const selection = document.getElementById('teamSelect');
 
-uba.addEventListener("click", myFunction);
-fed.addEventListener("click", myFunction);
+m2x.addEventListener('click', ubachangeTeam);
+cf.addEventListener('click', fedchangeTeam);
+uba.addEventListener('click', backFunctionUBA);
+fed.addEventListener('click', backFunctionFed);
 
-function myFunction() {
-  console.log('Clicked Back Button');
+function ubachangeTeam() {
+	console.log('Team Change');
+	selection.style.display = 'none';
+	ubaScores.style.display = 'flex';
+}
+
+function fedchangeTeam() {
+	console.log('Team Change');
+	selection.style.display = 'none';
+	fedScores.style.display = 'flex';
+}
+
+function backFunctionUBA() {
+	console.log('Clicked Back Button');
+	selection.style.display = 'flex';
+	ubaScores.style.display = 'none';
+}
+
+function backFunctionFed() {
+	console.log('Clicked Back Button');
+	selection.style.display = 'flex';
+	fedScores.style.display = 'none';
 }
