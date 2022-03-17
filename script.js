2edd5a526872b4ee5a1476a6bde45dcaa4ce7396
Denise Kurek <denisekurek@rocketmail.com>
2022-03-17 15:14:14 -0400

    added functionality to all score buttons for both teams, mine and opponents. Given each button function the ability to read from the text boxes for each score and handicap.

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ const uba = document.getElementById('backButtonUBA');
 const fed = document.getElementById('backButtonFED');
 const selection = document.getElementById('teamSelect');
 const ubaScratch = document.getElementById('ubaScratch');
-// const ubaHand1 = document.getElementById('ubaHand1');
-// const ubaHand2 = document.getElementById('ubaHand2');
+const ubaHandicap1 = document.getElementById('ubaHandi1');
+const ubaHandicap2 = document.getElementById('ubaHandi2');
+const fedScratch = document.getElementById('fedScratch');
+const fedHybrid = document.getElementById('fedHybrid');
+const fedHandicap = document.getElementById('fedHandicap');
+let m2xScore = 0;
+// let cfScore = 0;
+let oppScore = 0;
+let m2xHandicap = 0;
+let oppHandicap = 0;
 
 let m2xScratchTotal = 0;
 let oppScratchTotal = 0;
@@ -17,6 +25,11 @@ cf.addEventListener('click', fedchangeTeam);
 uba.addEventListener('click', backFunctionUBA);
 fed.addEventListener('click', backFunctionFed);
 ubaScratch.addEventListener('click', ubaCalcScr);
+ubaHandicap1.addEventListener('click', ubaCalcHandi1);
+ubaHandicap2.addEventListener('click', ubaCalcHandi2);
+fedScratch.addEventListener('click', fedCalcScr);
+fedHybrid.addEventListener('click', fedCalcHybrid);
+fedHandicap.addEventListener('click', fedCalcHandicap);
 
 function ubachangeTeam() {
 	// console.log('Team Change');
@@ -44,4 +57,44 @@ function backFunctionFed() {
 
 function ubaCalcScr() {
 	console.log('Uba Scratch');
+	m2xScore = document.getElementById('m2xScratchScore').value;
+	console.log(m2xScore);
+	oppScore = document.getElementById('oppScratchScore').value;
+	console.log(oppScore);
+}
+
+function ubaCalcHandi1() {
+	console.log('Uba Handicap 1');
+	m2xScore = document.getElementById('m2xHand1Score').value;
+	console.log(m2xScore);
+	m2xHandicap = document.getElementById('m2xHand1Cap').value;
+	console.log(m2xHandicap);
+	oppScore = document.getElementById('oppHand1Score').value;
+	console.log(oppScore);
+	oppHandicap = document.getElementById('oppHand1Cap').value;
+	console.log(oppHandicap);
+}
+
+function ubaCalcHandi2() {
+	console.log('Uba handicap 2');
+	m2xScore = document.getElementById('m2xHand2Score').value;
+	console.log(m2xScore);
+	m2xHandicap = document.getElementById('m2xHand2Cap').value;
+	console.log(m2xHandicap);
+	oppScore = document.getElementById('oppHand2Score').value;
+	console.log(oppScore);
+	oppHandicap = document.getElementById('oppHand2Cap').value;
+	console.log(oppHandicap);
+}
+
+function fedCalcScr() {
+	console.log('Fed Scratch');
+}
+
+function fedCalcHybrid() {
+	console.log('Fed Hybrid');
+}
+
+function fedCalcHandicap() {
+	console.log('Fed Handicap');
 }
