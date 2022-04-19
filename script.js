496e438d5d20130e221c1f182468c62fef7ea1e2
denisekurek <denisekurek@rocketmail.com>
2022-04-18 21:32:40 -0400

    Commented out JS code  that did not deal with the UBA scratch pair. Added an individual function to calculate scores and who won each game. Created a list within the original function as to what needs to be done to complete the entire process.

--- script.js
+++ script.js
@@ -14,31 +14,32 @@ const fedHandicap = document.getElementById('fedHandicap');
 let m2xScore = 0;
 let cfScore = 0;
 let oppScore = 0;
+let winner;
 let m2xHandicap = 0;
-let cfHandicap = 0;
+// let cfHandicap = 0;
 let oppHandicap = 0;
-let ubaScoreCount = 2;
-let fedScoreCount = 1;
+// let ubaScoreCount = 2;
+// let fedScoreCount = 1;
 let pair1Game = 1;
-let pair2Game = 1;
-let pair3Game = 1;
+// let pair2Game = 1;
+// let pair3Game = 1;
 let pair1Total = 0;
-let pair2Total = 0;
-let pair3Total = 0;
+// let pair2Total = 0;
+// let pair3Total = 0;
 let pair1TotalOpp = 0;
-let pair2TotalOpp = 0;
-let pair3TotalOpp = 0;
+// let pair2TotalOpp = 0;
+// let pair3TotalOpp = 0;
 
 m2x.addEventListener('click', ubachangeTeam);
 cf.addEventListener('click', fedchangeTeam);
 uba.addEventListener('click', backFunctionUBA);
 fed.addEventListener('click', backFunctionFed);
 ubaScratch.addEventListener('click', ubaCalcScr);
-ubaHandicap1.addEventListener('click', ubaCalcHandi1);
-ubaHandicap2.addEventListener('click', ubaCalcHandi2);
-fedScratch.addEventListener('click', fedCalcScr);
-fedHybrid.addEventListener('click', fedCalcHybrid);
-fedHandicap.addEventListener('click', fedCalcHandicap);
+// ubaHandicap1.addEventListener('click', ubaCalcHandi1);
+// ubaHandicap2.addEventListener('click', ubaCalcHandi2);
+// fedScratch.addEventListener('click', fedCalcScr);
+// fedHybrid.addEventListener('click', fedCalcHybrid);
+// fedHandicap.addEventListener('click', fedCalcHandicap);
 
 function ubachangeTeam() {
 	// console.log('Team Change');
@@ -88,87 +89,94 @@ function backFunctionFed() {
 	oppHandicap = 0;
 }
 
-function ubaCalcScr() {
-	// console.log('Uba Scratch');
-	m2xScore = parseInt(document.getElementById('m2xScratchScore').value);
-	// console.log(m2xScore);
-	oppScore = parseInt(document.getElementById('oppScratchScoreUBA').value);
-	// console.log(oppScore);
-
-	//Determine the winner of the game
-	if (m2xScore > oppScore) {
+function CalculateUBAScores(motionScore, motionHandicap, oppScore, oppHandicap) {  //Return winner and total scores
+	if (motionScore > oppScore) {
 		console.log('M2X Wins Game');
-	} else if (oppScore > m2xScore) {
+		return '1'
+	} else if (oppScore > motionScore) {
 		console.log('Opp Wins Game');
+		return '-1`'
 	} else {
 		console.log('Draw');
+		return '0'
 	}
-	//Remove scores from previous game
-	pair1Total += m2xScore;
+}
 
+function ubaCalcScr() {
+	// Get value for each teams scores
+	m2xScore = parseInt(document.getElementById('m2xScratchScore').value);
+	oppScore = parseInt(document.getElementById('oppScratchScoreUBA').value);
+
+	//Calculating who won the game with scores
+	CalculateUBAScores(m2xScore, m2xHandicap, oppScore, oppHandicap)
+
+	//Update Points won and Team Totals and change page elements to reflect new totals
+	pair1Total += m2xScore;
 	pair1TotalOpp += oppScore;
-	//Add Totals to page
 	console.log('M2X total: ' + pair1Total);
 	console.log('Opp Total: ' + pair1TotalOpp);
-	//Update Game being displayed
+
+	//Update Game being displayed - complete
 	let game = document.getElementById('ubaScratchGame');
 	pair1Game++;
 	console.log('Game:' + pair1Game);
 	game.innerHTML = pair1Game.toString();
+	
+	//Remove scores from previous game
 }
 
-function ubaCalcHandi1() {
-	console.log('Uba Handicap 1');
-	m2xScore = document.getElementById('m2xHand1Score').value;
-	console.log(m2xScore);
-	m2xHandicap = document.getElementById('m2xHand1Cap').value;
-	console.log(m2xHandicap);
-	oppScore = document.getElementById('oppHand1ScoreUBA').value;
-	console.log(oppScore);
-	oppHandicap = document.getElementById('oppHand1CapUBA').value;
-	console.log(oppHandicap);
-}
+// function ubaCalcHandi1() {
+// 	console.log('Uba Handicap 1');
+// 	m2xScore = document.getElementById('m2xHand1Score').value;
+// 	console.log(m2xScore);
+// 	m2xHandicap = document.getElementById('m2xHand1Cap').value;
+// 	console.log(m2xHandicap);
+// 	oppScore = document.getElementById('oppHand1ScoreUBA').value;
+// 	console.log(oppScore);
+// 	oppHandicap = document.getElementById('oppHand1CapUBA').value;
+// 	console.log(oppHandicap);
+// }
 
-function ubaCalcHandi2() {
-	console.log('Uba handicap 2');
-	m2xScore = document.getElementById('m2xHand2Score').value;
-	console.log(m2xScore);
-	m2xHandicap = document.getElementById('m2xHand2Cap').value;
-	console.log(m2xHandicap);
-	oppScore = document.getElementById('oppHand2ScoreUBA').value;
-	console.log(oppScore);
-	oppHandicap = document.getElementById('oppHand2CapUBA').value;
-	console.log(oppHandicap);
-}
+// function ubaCalcHandi2() {
+// 	console.log('Uba handicap 2');
+// 	m2xScore = document.getElementById('m2xHand2Score').value;
+// 	console.log(m2xScore);
+// 	m2xHandicap = document.getElementById('m2xHand2Cap').value;
+// 	console.log(m2xHandicap);
+// 	oppScore = document.getElementById('oppHand2ScoreUBA').value;
+// 	console.log(oppScore);
+// 	oppHandicap = document.getElementById('oppHand2CapUBA').value;
+// 	console.log(oppHandicap);
+// }
 
-function fedCalcScr() {
-	console.log('Fed Scratch');
-	cfScore = document.getElementById('cfScratchScore').value;
-	console.log(cfScore);
-	oppScore = document.getElementById('oppScratchScoreFed').value;
-	console.log(oppScore);
-}
+// function fedCalcScr() {
+// 	console.log('Fed Scratch');
+// 	cfScore = document.getElementById('cfScratchScore').value;
+// 	console.log(cfScore);
+// 	oppScore = document.getElementById('oppScratchScoreFed').value;
+// 	console.log(oppScore);
+// }
 
-function fedCalcHybrid() {
-	console.log('Fed Hybrid');
-	cfScore = document.getElementById('cfHybridScratchScore').value;
-	console.log(cfScore);
-	cfHandicap = document.getElementById('cfHybridCap').value;
-	console.log(cfHandicap);
-	oppScore = document.getElementById('oppHybridScratchScoreFed').value;
-	console.log(oppScore);
-	oppHandicap = document.getElementById('oppHybridCapFed').value;
-	console.log(oppHandicap);
-}
+// function fedCalcHybrid() {
+// 	console.log('Fed Hybrid');
+// 	cfScore = document.getElementById('cfHybridScratchScore').value;
+// 	console.log(cfScore);
+// 	cfHandicap = document.getElementById('cfHybridCap').value;
+// 	console.log(cfHandicap);
+// 	oppScore = document.getElementById('oppHybridScratchScoreFed').value;
+// 	console.log(oppScore);
+// 	oppHandicap = document.getElementById('oppHybridCapFed').value;
+// 	console.log(oppHandicap);
+// }
 
-function fedCalcHandicap() {
-	console.log('Fed Handicap');
-	m2xScore = document.getElementById('cfHandScore').value;
-	console.log(m2xScore);
-	m2xHandicap = document.getElementById('cfHandCap').value;
-	console.log(m2xHandicap);
-	oppScore = document.getElementById('oppHandScoreFed').value;
-	console.log(oppScore);
-	oppHandicap = document.getElementById('oppHandCapFed').value;
-	console.log(oppHandicap);
-}
+// function fedCalcHandicap() {
+// 	console.log('Fed Handicap');
+// 	m2xScore = document.getElementById('cfHandScore').value;
+// 	console.log(m2xScore);
+// 	m2xHandicap = document.getElementById('cfHandCap').value;
+// 	console.log(m2xHandicap);
+// 	oppScore = document.getElementById('oppHandScoreFed').value;
+// 	console.log(oppScore);
+// 	oppHandicap = document.getElementById('oppHandCapFed').value;
+// 	console.log(oppHandicap);
+// }
